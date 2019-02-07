c8f8379584dd35a1a69f614f925528a8910f9046
Luciano Rossi <lukio@gcoop.coop>
2019-02-07 04:43:18 -0300

    flake8

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
-#This file is part sale_pos_ar module for Tryton.
-#The COPYRIGHT file at the top level of this repository contains
-#the full copyright notices and license terms.
+# This file is part sale_pos_ar module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains
+# the full copyright notices and license terms.
 from trytond.pool import Pool
 from . import sale
 from . import configuration
--- sale.py
+++ sale.py
@@ -1,5 +1,5 @@
-#The COPYRIGHT file at the top level of this repository contains
-#the full copyright notices and license terms.
+# The COPYRIGHT file at the top level of this repository contains
+# the full copyright notices and license terms.
 
 from trytond.model import fields
 from trytond.pool import Pool, PoolMeta
