e94353f28073f2417d794e5bf839945e04ae9c95
Luciano Rossi <lukio@gcoop.coop>
2019-03-27 11:42:50 -0300

    Update tests. Add scenario

--- tests/test_sale_pos_ar.py
+++ tests/test_sale_pos_ar.py
@@ -1,9 +1,12 @@
 # The COPYRIGHT file at the top level of this repository contains the
 # full copyright notices and license terms.
 import unittest
+import doctest
 
 from trytond.tests.test_tryton import ModuleTestCase
 from trytond.tests.test_tryton import suite as test_suite
+from trytond.tests.test_tryton import doctest_teardown
+from trytond.tests.test_tryton import doctest_checker
 
 
 class SalePosArTestCase(ModuleTestCase):
@@ -15,4 +18,8 @@ def suite():
     suite = test_suite()
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
             SalePosArTestCase))
+    suite.addTests(doctest.DocFileSuite('scenario_sale_pos_ar.rst',
+            tearDown=doctest_teardown, encoding='utf-8',
+            checker=doctest_checker,
+            optionflags=doctest.REPORT_ONLY_FIRST_FAILURE))
     return suite
