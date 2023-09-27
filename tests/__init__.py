20fa7e79516b7d996a41985212b11c7be886876d
Adrián Bernardi <adrian.bernardi@gmail.com>
2023-09-26 23:20:38 -0300

    Replace test setuptools command by unittest discover

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,9 +1,2 @@
-# The COPYRIGHT file at the top level of jhis repository contains the
-# full copyright notices and license terms.
-
-try:
-    from trytond.modules.sale_pos_ar.tests.test_sale_pos_ar import suite
-except ImportError:
-    from .test_sale_pos_ar import suite
-
-__all__ = ['suite']
+# This file is part of Tryton.  The COPYRIGHT file at the top level of
+# this repository contains the full copyright notices and license terms.
