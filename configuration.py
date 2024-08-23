3b8852ae4b5fa966fcf6436582c53889fa65b844
Luciano Rossi <lukio@gcoop.coop>
2024-08-23 12:02:40 -0300

    Remove migrations previous to 5.0
    more info:
    https://github.com/tryton/tryton/commit/960c2203294a517b0df798d22965d2a3b71430f6

--- configuration.py
+++ configuration.py
@@ -4,8 +4,6 @@
 
 from trytond.model import fields, ModelSQL
 from trytond.pool import PoolMeta, Pool
-from trytond import backend
-from trytond.tools.multivalue import migrate_property
 from trytond.modules.company.model import CompanyValueMixin
 
 
@@ -31,19 +29,3 @@ class ConfigurationPos(ModelSQL, CompanyValueMixin):
     pos = fields.Many2One('account.pos', "Point of Sale", required=True,
         domain=[('pos_daily_report', '=', False)],
         depends={'company'})
-
-    @classmethod
-    def __register__(cls, module_name):
-        exist = backend.TableHandler.table_exist(cls._table)
-        super().__register__(module_name)
-        if not exist:
-            cls._migrate_property([], [], [])
-
-    @classmethod
-    def _migrate_property(cls, field_names, value_names, fields):
-        field_names.append('pos')
-        value_names.append('pos')
-        fields.append('company')
-        migrate_property(
-            'sale.configuration', field_names, cls, value_names,
-            fields=fields)
